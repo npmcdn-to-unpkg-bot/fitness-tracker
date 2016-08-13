b45ee20d5f3bd5ce17634373c5dd63e903a9ab08
Joyce Lin <joycelin79@gmail.com>
2016-08-13 14:11:19 -0700

    organize directories

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,13 +2,19 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Main = React.createClass({
-    render = function() {
+    render: function() {
         return (
-            <div>
-                Hello World
+            <div className='message'>
+                <h3>the grooviest workout app for all seasons and for all reasons</h3>
+                <ul>
+                    <li>exercises</li>
+                    <li>equipment</li>
+                    <li>workouts</li>
+                    <li>save, social, find?</li>
+                </ul>
             </div>
         )
     }
-})
+});
 
 ReactDOM.render(<Main />, document.getElementById('app'));
